2193eab041ef402876b3d5aa60152313af255300
Heboxin <heboxin@pku.edu.cn>
2018-05-08 09:00:09 +0800

    change mpirun method

--- tests_mpi/mpi_speed.cpp
+++ tests_mpi/mpi_speed.cpp
@@ -34,14 +34,18 @@ int main (int argc, char *argv[])
     MPI_Status status;
     double start_time = get_curtime();
     for(int i = 0;i < 100;++i){
-        MPI_Isend(send_data, send_bytes, MPI_CHAR, peerrank, 123, MPI_COMM_WORLD, &send_handler);
-        MPI_Irecv(recv_data, send_bytes, MPI_CHAR, peerrank, 123, MPI_COMM_WORLD, &recv_handler);
-        MPI_Wait(&send_handler, &status);
-        MPI_Wait(&recv_handler, &status);
+        //if (myrank ==0)
+            MPI_Isend(send_data, send_bytes, MPI_CHAR, peerrank, 123, MPI_COMM_WORLD, &send_handler);
+        //else
+            MPI_Irecv(recv_data, send_bytes, MPI_CHAR, peerrank, 123, MPI_COMM_WORLD, &recv_handler);
+        //if (myrank == 0)
+            MPI_Wait(&send_handler, &status);
+        //else
+            MPI_Wait(&recv_handler, &status);
         //printf("[rank %d] iter %d.\n", myrank, i);
     }
     double consume_time = get_curtime() - start_time;
-    size_t total_bytes = (size_t)100 * send_bytes* 2;
+    size_t total_bytes = (size_t)100 * send_bytes * 2;
     double speed = (double)total_bytes/1024/1024/consume_time;
     printf("[rank %d] tranfer_size:%lld consume_time:%.6lf speed:%.2lfMBytes/sec %.6lf iters/sec\n",
          myrank, (long long)total_bytes, consume_time, speed, (double)100/consume_time);
